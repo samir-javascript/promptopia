9e8de221f35ad92a42e3f99c17c5e8bb4e7a6792
samir-javascript <samirjavascript00@gmail.com>
2023-11-25 11:09:28 +0100

    change some thing

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,7 +9,7 @@ export const GET = async (request) => {
 
         // Fetch prompts and sort by the latest ones using _id in descending order
         const prompts = await Prompt.find({}).sort({ _id: -1 }).populate('creator');
-        console.log(prompts);
+      
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
