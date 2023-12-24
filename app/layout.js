6974399a58bf1500a1e6bd7e41b7e992e1691fb4
samir-javascript <samirjavascript00@gmail.com>
2023-12-24 23:40:48 +0100

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,12 @@
 import Provider from '@/components/Provider';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css'
-
+import Head from 'next/head';
 export const metadata = {
   title: "Promtopia",
   description:
     "Q&A community for programmers , solving coding challenges, sharing knowledge.",
-  icons: {
-    icon: "/assets/images/logo.svg",
-  },
+  
 };
 
 
@@ -16,6 +14,12 @@ export default function RootLayout({children}) {
   return (
    
       <html lang="en">
+        <Head>
+        
+        
+        {/* Add the following line to include your favicon */}
+        <link rel="icon" href="/assets/images/logo.svg" />
+      </Head>
         <body>
         <Provider>  
           {children}
